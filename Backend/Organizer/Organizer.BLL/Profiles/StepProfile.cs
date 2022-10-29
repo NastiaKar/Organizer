63e140eb1fe1788f364b86c5c5b5a665ace208fa
NastiaKar <nastia.karpyuk@outlook.com>
2022-10-29 12:21:24 +0300

    modified StepProfile

--- Backend/Organizer/Organizer.BLL/Profiles/StepProfile.cs
+++ Backend/Organizer/Organizer.BLL/Profiles/StepProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace Organizer.BLL.Profiles;
+﻿using AutoMapper;
+using Organizer.DAL.Entities;
+using Organizer.Models.DTOs.Step;
 
-public class StepProfile
+namespace Organizer.BLL.Profiles;
+
+public class StepProfile : Profile
 {
-    
+    public StepProfile()
+    {
+        CreateMap<CreateStepDTO, Step>();
+        CreateMap<UpdateStepDTO, Step>();
+        CreateMap<Step, DisplayStepDTO>();
+    }
 }
