834b60b05e7ba5144d0068fcdf7b61d68f053d55
NastiaKar <nastia.karpyuk@outlook.com>
2022-10-29 12:49:11 +0300

    modified BoardProfile

--- Backend/Organizer/Organizer.BLL/Profiles/BoardProfile.cs
+++ Backend/Organizer/Organizer.BLL/Profiles/BoardProfile.cs
@@ -1,6 +1,22 @@
-﻿namespace Organizer.BLL.Profiles;
+﻿using AutoMapper;
+using Organizer.DAL.Entities;
+using Organizer.Models.DTOs.Assignment;
+using Organizer.Models.DTOs.Board;
 
-public class BoardProfile
+namespace Organizer.BLL.Profiles;
+
+public class BoardProfile : Profile
 {
-    
+    public BoardProfile()
+    {
+        var mapper = new Mapper(new MapperConfiguration(config =>
+            config.AddProfiles(new Profile[] {new AssignmentProfile(), new StepProfile()})));
+
+        CreateMap<CreateBoardDTO, Board>();
+
+        CreateMap<UpdateBoardDTO, Board>();
+
+        CreateMap<Board, DisplayBoardDTO>().ForMember(dest => dest.Assignments,
+            options => options.MapFrom(b => mapper.Map<List<DisplayAssignmentDTO>>(b.Assignments)));
+    }
 }
